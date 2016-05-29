2a25e6c2b2fb39c91dbbd1ac2da14a8ddfd0367f
Richard <richardwangwang@gmail.com>
2016-05-29 23:04:14 +0800

    Update tag string generator.

--- tagging_app/tagging_app_utils.py
+++ tagging_app/tagging_app_utils.py
@@ -3,8 +3,8 @@ from django.contrib.contenttypes.models import ContentType
 
 def generate_html_attributes(obj):
     tag_str = get_tag_str(obj)
-    return ' objectId="%d" tags="%s" contentType="%d"' % (obj.pk, tag_str,
-                                                          ContentType.objects.get_for_model(obj).pk)
+    return ' objectId="%d" tags="%s" contentType="%d"' % (
+        obj.pk, tag_str, ContentType.objects.get_for_model(obj).pk)
 
 
 def get_tag_str(obj):
@@ -13,8 +13,16 @@ def get_tag_str(obj):
     return tag_str
 
 
+def get_tag_str_from_tag_list(tag_list):
+    return ",".join(get_tags_from_tag_list(tag_list))
+
+
 def get_tags(obj):
+    return get_tags_from_tag_list(obj.tags)
+
+
+def get_tags_from_tag_list(tag_list):
     tag_name_list = []
-    for tag in obj.tags:
+    for tag in tag_list:
         tag_name_list.append(tag.name)
     return tag_name_list
